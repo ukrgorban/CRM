bb8bffce6c36f1efa90de37bb0d96be506084821
Alexandr Gorban <ukrgorban@gmail.com>
2018-07-11 16:54:53 +0300

    registration backend

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,6 @@
+﻿const User = require('../model/User');
+const bcrypt = require('bcryptjs');
+
 module.exports.login = function (req, res) {
     res.status(200).json({
         //login: 'login from controller'
@@ -5,8 +8,29 @@ module.exports.login = function (req, res) {
     });
 };
 
-module.exports.register = function (req, res) {
-    res.status(200).json({
-        login: 'register from controller'
-    });
+module.exports.register = async function (req, res) {
+    const candidate = await User.findOne({ email: req.body.email });
+
+    if (candidate) {
+        res.status(409).json({
+            message: 'Такой email уже занят. Попробуйте другой.'
+        });
+    } else {
+        const salt = bcrypt.genSaltSync(10);
+        const password = req.body.password;
+
+        const user = new User({
+            email: req.body.email,
+            password: bcrypt.hashSync(password, salt)
+        });
+
+        try {
+            await user.save();
+            res.status(201).json({
+                message: 'Пользователь успешно создан'
+            });
+        } catch (err) {
+            console.log(err);
+        }
+    }
 };
--- model/User.js
+++ model/User.js
@@ -8,7 +8,7 @@ const userSchema = new Schema({
         required: true
     },
     password: {
-        type: Number,
+        type: String,
         required: true
     }
 });
